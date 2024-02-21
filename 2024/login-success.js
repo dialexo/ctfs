b0f31e98b570fc6780c11598782a9f143c9b2444
Alexandre Chau <alexandre@pocketcampus.org>
2024-02-21 16:59:42 +0100

    fix: url param and results filtering in checker

--- 2024/login-success.js
+++ 2024/login-success.js
@@ -4,16 +4,27 @@ const text = fs.readFileSync('../.pm2/logs/ctf-out.log', { encoding: 'utf8' })
 
 const lines = text.split('\n')
 
-const successes = lines.filter(l => l.match(/.*\/login.*null.*/)).filter(l => l.match('token')).map(l => decodeURIComponent(l)).map(l => l.match(/\{.*\}/)).filter(e => e !== null).filter(l => { try { JSON.parse(l[0]); return true } catch { return false } }).map(l => [l['input'], JSON.parse(l[0])]).filter(e => e[1].value === null)
+const successes = lines.filter(l => l.match(/.*\/login.*null.*/))
+    .filter(l => l.match('token'))
+    .filter(l => !l.includes('token=true'))
+    .map(l => decodeURIComponent(l))
+    .map(l => l.match(/\{.*\}/))
+    .filter(e => e !== null).filter(l => { try { JSON.parse(l[0]); return true } catch { return false } })
+    .map(l => [l['input'], JSON.parse(l[0])]).filter(e => e[1].value === null)
 
-const names = successes.map(([l]) => l.match(/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/)).filter(e => e).map(e => e[0])
+const names = successes.map(([l]) => l.match(/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/))
+    .filter(e => e)
+    .map(e => e[0])
 
 const ids = [...new Set(names)]
 
 const state = fs.readFileSync('state', { encoding: 'utf8' })
 
-const pairs = state.split('\n').filter(e => e).map(e => e.split(':')).map(e => [e[0], e[1]])
+const pairs = state.split('\n')
+    .filter(e => e)
+    .map(e => e.split(':'))
+    .map(e => [e[0], e[1]])
 
 let map = new Map(pairs)
 
-console.log(ids.map(i => map.get(i)))
+console.log(ids.map(i => map.get(i)).filter(Boolean))
